57f2e4bb886914581718fb5714ab621ec8485c0a
hackbr0wn <rajeevlochan2@gmail.com>
2025-02-10 13:42:09 -0600

    updated index.py

--- api/index.py
+++ api/index.py
@@ -4,5 +4,6 @@ from horoscope_api.wsgi import application  # Import the WSGI application from y
 # Ensure that Django settings are properly configured for Vercel
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'horoscope_api.settings')
 
-# Vercel will use this application callable to process requests
+# Vercel will use this `app` callable to process requests
+app = application  # Add this line to explicitly assign the WSGI application to `app`
 
